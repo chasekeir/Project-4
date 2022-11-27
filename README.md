740476785116ff4660c7847dbdb5b68bdc08617a
chasekeir <112430451+chasekeir@users.noreply.github.com>
2022-11-27 12:10:49 -0800

    6

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Project-4
  
+
+
 ```
 len(comments)= 1000
 len(top_level_comments)= 1000
@@ -11,3 +13,4 @@ len(valid_replies)= 0
 valid_comments= 1000
 ========================================
 ```
+I completed the 6 tasks in the <code>bot.py</code> file for the 12 points it was worth. I also completed this github repo for the 3 points it was worth. I also completed recieving the 1000 valid comments as can be seen above so that rewards me another 10 points. I completed the <code>bot_vote.py</code> file and use the textblob sentiment analysis for the 4 extra credit points it is worth and let it run to upvote comments and submissions related to Biden. I also completed the <code>bot_submissions.py</code> for the 2 extra credit points it was worth. I drew my submissions from the "Liberal" subreddit. Overall, my score should be a 31/30 for the work I put in on the required steps as well as the extra credits I was able to complete.
--- bot_vote.py
+++ bot_vote.py
@@ -1,31 +1,42 @@
 import praw
-from textblob import TextBlob 
+from textblob import TextBlob
 
 reddit = praw.Reddit('bot')
 
+key = ['biden']
 
 subreddit = reddit.subreddit('cs40_2022fall')
+while True:
+    for submission in list((reddit.subreddit("cs40_2022fall").hot(limit=None))):
+        submission.comments.replace_more(limit=None)
+        normalized_title = submission.title.lower()
+        normalized_comments = submission.comments.list()
+        for phrase in key:
+            if phrase in normalized_title:
+                textblob=TextBlob(submission.title)
+                polarity=textblob.sentiment.polarity
+                print('polarity=',polarity)
+                if polarity>=0.0:
+                    submission.upvote()
+                    print('Submission Upvoted') 
+                    break
+                else:
+                    submission.downvote()
+                    print('Submission Downvoted')
+                    break 
 
-for submission in subreddit.top(limit=None):
-    submission.comments.replace_more(limit=None)
-    all_comments = submission.comments.list()
-
-    not_my_comments = []
-
-    for comment in all_comments:
-        try:
-            if comment.author=="botfly123":
-                pass
-            else:
-                not_my_comments.append(comment)
-                if "Biden" in comment.body:
-                    blob= TextBlob(comment.body)
-                    if blob.sentiment.polarity>0:
+        for comment in normalized_comments:
+            lowercase = comment.body.lower()
+            for phrase in key:
+                if phrase in lowercase:
+                    textblob=TextBlob(comment.body)
+                    polarity=textblob.sentiment.polarity
+                    print('polarity=',polarity)
+                    if polarity>=0.0:
                         comment.upvote()
-                        print('upvote: ',comment.title)
+                        print('Comment Upvoted') 
+                        break
                     else:
-                        comment.downvote()  
-                        print('downvote: ',comment.title)      
-        except AttributeError:
-            print('not a comment')
-    
+                        comment.downvote()
+                        print('Comment Downvoted')
+                        break 
